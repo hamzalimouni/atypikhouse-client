8d08eaca019c057aced50c42bae0e0eedaf4f6e4
ELYAMOUNI <hamzalimouni9@gmail.com>
2022-11-07 14:43:00 +0100

    update: search item component

--- src/components/SearchItem.jsx
+++ src/components/SearchItem.jsx
@@ -1,23 +1,30 @@
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import { Col, Row } from 'react-bootstrap'
 import '../assets/css/searchItem.css'
 
-const SearchItem = () => {
+const SearchItem = (props) => {
+    const image = "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aG91c2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60";
     return (
-        <div className='search-item my-2 d-flex rounded'>
-            <img className='search-img rounded' src="https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aG91c2VzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="" />
-            <div className="search-description d-flex justify-content-center flex-column mx-3">
-                <h2 className='mb-1'>Hôtel Opéra Liège</h2>
-                <span className='search-subtitle mb-2'>Premier hôtel zero plastiquie à Paris</span>
-                <span className='search-location'><FontAwesomeIcon className='icon' icon={faLocationDot} /> Paris, Département de Paris, Île-de-France</span>
-                <div className="search-info pb-3 d-flex justify-content-between align-items-end">
+        <div className='search-item-card my-2 rounded'>
+            <div className="house-images">
+                <img className='rounded' src={image} alt="" />
+            </div>
+            <div className="house-details">
+                <h2>Hôtel Opéra Liège{props.title}</h2>
+                <p>Lorem ipsum dolor sit amet adipisicing elit, consectetur adipisicing elit.{props.description}</p>
+                <div className="location">
+                    <FontAwesomeIcon className='icon' icon={faLocationDot} color="#8ED081" />
+                    <h3>Puisseguin, Gironde, Nouvelle-Aquitaine{props.location}</h3>
+                </div>
+                <div className="search-info">
                     <div className="search-detail">
-                        <span>1 logement</span>
-                        <span>, 2 personnes</span>
+                        <span>1{props.logement} logement</span>
+                        <span>, 2{props.personnes} personnes</span>
                     </div>
                     <div className="search-price">
-                        <span>235 €</span>/nuit
+                        <span>235{props.price} €</span>/nuit
                     </div>
                 </div>
             </div>
