d744bdbad785037bbb1b81c3813eed0c602f2802
Mohammed Moudou <medgeek7@gmail.com>
2023-01-12 22:13:47 +0100

    Update Variables.js

--- src/Variables.js
+++ src/Variables.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8000/api'
+export const API_URL = 'https://atypikhouseapi.azurewebsites.net/public/index.php/api/'
 export const MEDIA_URL = 'https://atypikhouseapi.azurewebsites.net/public/media/'
