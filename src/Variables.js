a71ab2ba352ac3ada53f6301fe22d30f32da6200
hamzalimouni <hamzalimouni9@gmail.com>
2024-04-25 12:23:38 +0200

    update variable files

--- src/Variables.js
+++ src/Variables.js
@@ -1,2 +1,3 @@
-export const API_URL = 'https://atypikhouseapi.azurewebsites.net/public/index.php/api/'
+// export const API_URL = 'https://atypikhouseapi.azurewebsites.net/public/index.php/api/'
+export const API_URL = 'http://localhost:8000/api'
 export const MEDIA_URL = 'https://atypikhouseapi.azurewebsites.net/public/media/'
